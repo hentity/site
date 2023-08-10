b018892dcd8c330ba283b9fa2a60f0c6b798dc3c
henryhamer <103616397+hentity2@users.noreply.github.com>
2023-08-10 15:09:29 +1000

    md conversion and routing

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,44 @@
+import { getAllPostIds, getPostData } from '@/lib/posts'
+
+type Params = {
+  id: string
+}
+
+type Props = {
+  params: Params
+}
+
+type PostData = {
+  title: string
+  date: string
+  contentHtml: string
+}
+
+export async function generateMetadata({ params }: Props) {
+  const postData: PostData = await getPostData(params.id)
+
+  return {
+    title: postData.title,
+  }
+}
+
+// -< Post >-
+export default async function Post({ params }: Props) {
+  const postData: PostData = await getPostData(params.id)
+
+  return (
+    <>
+      {/* Post Title */}
+      <h1>{postData.title}</h1>
+
+      <div>
+        {postData.date}
+      </div>
+
+      {/* Post Content */}
+      <div
+        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+      />
+    </>
+  )
+}
